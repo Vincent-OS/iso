61f6b8b27e6ce096f8c58de05d36a809e4d2c528
Florian. M <florian.m@v38armageddon.net>
2025-11-19 21:00:23 +0100

    [ARCHISO] Publish files for Vincent OS 1.1 Release

--- src/profiledef.sh
+++ src/profiledef.sh
@@ -2,15 +2,13 @@
 # shellcheck disable=SC2034
 
 iso_name="vincentOS"
-iso_label="VINCENTOS-1.0_origin"
+iso_label="VINCENTOS-1.1_origin"
 iso_publisher="v38armageddon <https://vincent-os.v38armageddon.net>"
 iso_application="Vincent OS LiveCD"
-iso_version="1.0"
+iso_version="1.1"
 install_dir="vincentos"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.systemd-boot.esp' 'uefi-x64.systemd-boot.esp'
-           'uefi-ia32.systemd-boot.eltorito' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux' 'uefi.systemd-boot')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
@@ -22,6 +20,7 @@ file_permissions=(
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
+  ["/usr/local/bin/boot-mode.sh"]="0:0:755"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/etc/sudoers"]="0:0:440"
 )
